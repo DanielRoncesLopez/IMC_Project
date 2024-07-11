3fd3e25aef11b220adbbb3770c813a5364203882
Daniel Ronces López <danielronces@hotmail.com>
2024-07-11 14:15:49 -0600

    Project start

---
+++ InputData.py
@@ -0,0 +1,4 @@
+class InputPatienceData():
+    def __init__(self):
+        self.name = str(input("Patience name: "))
+        self.age = int(input("Patience age: "))
---
+++ Main.py
@@ -0,0 +1,5 @@
+import InputData
+
+class Main(): 
+    persona = InputData.InputPatienceData()
+    print("Patience Data: ", persona.name, persona.age)
